52ea86c57ebf71bfc7794026c785e85430768412
nearlynithin <nearlynithin@gmail.com>
2025-02-19 22:57:43 +0530

    feat: updated chessboard blueprint

--- chess_board.py
+++ chess_board.py
@@ -1,7 +1,19 @@
 UNITS = 5 # temp units for the scaling of the board
+dirs = {"up":(-1,0),"down":(+1,0),"left":(0,-1),"right":(0,1),"top_left":(-1,-1),"top_right":(-1,1),"bottom_left":(1,-1),"bottom_right":(1,1)}
+state = {}
 
 
 class ChessBoard:
+    
+    def __init__(self):
+        self.pieces = {}
+        self.initialize_pieces()
+    
+    def initialize_pieces(self):
+        pass
+    
+    def draw_board(self):
+        pass
     
     def draw_board(self):
         pass
