58bbcce7d989fd28184b684055b0c7e1199a5c44
nearlynithin <nearlynithin@gmail.com>
2025-02-23 19:17:18 +0530

    fix: swapped indices while piece initialization and bound checking + Early break on Queen movement when not an oponent piece is encountered

--- chess_pieces.py
+++ chess_pieces.py
@@ -1,20 +1,24 @@
-import chess_board as board
-from chess_board import UNITS
+import pprint
+from pyray import *
+from chess_board import ChessBoard, UNITS
 
 
 class Piece:
-    def __init__(self, color, x, y):
+    def __init__(self, color, x, y, board):
         self.color = color
         self.x = x
         self.y = y
         self.moves = []
-
-    def __del__(self):
-        # destructor
-        pass
+        self.t_color = WHITE
+        self.board = board  # Store reference to the board
 
     def move(self, x, y):
         if (x, y) in self.moves:
+            # Update board state
+            old_pos = (self.x, self.y)
+            new_pos = (x, y)
+            self.board.state[new_pos] = self
+            self.board.state[old_pos] = None
             self.x = x
             self.y = y
             if isinstance(self, Pawn):
@@ -24,117 +28,119 @@ class Piece:
         return (self.x, self.y)
 
     def init_texture(self):
-        # method to be overridden in every Piece's class
-        print("Initializing textures...")
-
-    def highlight(self):
-        pass
-
-    def get_moves(self):
         pass
 
 
 class Pawn(Piece):
-    def __init__(self, color, x, y):
-        super().__init__(color, x, y)
+    def __init__(self, color, x, y, board):
+        super().__init__(color, x, y, board)
         self.first = True
-
-    def move(self, x, y):
-        super().move(x, y)
+        self.t_color = WHITE
 
     def get_moves(self):
         self.moves.clear()
         opponent_color = "black" if self.color == "white" else "white"
-        dir = 1
-        if self.color == "black":
-            dir *= -1
 
-        # Forward moves
-        dx, dy = board.dirs["up"]
-        forward_pos = (self.x + dx*dir, self.y + dy*dir)
+        if (self.color == "white"):
+            dx, dy = self.board.dirs["up"]
+            next_row = self.x + dx
+            next_col = self.x + dy
+            next_pos = (next_row, next_col)
 
-        if forward_pos in board.state and board.state[forward_pos] is None:
-            self.moves.append(forward_pos)
-            if self.first:
-                double_forward = (self.x + dx*2*dir, self.y + dy*2*dir)
-                if double_forward in board.state and board.state[double_forward] is None:
-                    self.moves.append(double_forward)
+            print(self.board.state.get(next_pos))
 
-        # Capture moves
-        for capture_dir in ["top_left", "top_right"]:
-            dx, dy = board.dirs[capture_dir]
-            capture_pos = (self.x + dx*dir, self.y + dy*dir)
+            if 0 <= next_row < 8 and 0 <= next_col < 8 and self.board.state.get(next_pos) is None:
+                self.moves.append(next_pos)
+                if self.first:
+                    next_row = self.x + dx * 2
+                    next_col = self.x + dy * 2
+                    next_pos = (next_row, next_col)
+                    if 0 <= next_row < 8 and 0 <= next_col < 8 and self.board.state.get(next_pos) is None:
+                        self.moves.append(next_pos)
 
-            if capture_pos in board.state and board.state[capture_pos] is not None:
-                if board.state[capture_pos].color == opponent_color:
-                    self.moves.append(capture_pos)
+        if (self.color == "black"):
+            dx, dy = self.board.dirs["down"]
+            next_row = self.x + dx
+            next_col = self.x + dy
+            next_pos = (next_row, next_col)
+
+            if 0 <= next_row < 8 and 0 <= next_col < 8 and self.board.state.get(next_pos) is None:
+                self.moves.append(next_pos)
+                if self.first:
+                    next_row = self.x + dx * 2
+                    next_col = self.x + dy * 2
+                    next_pos = (next_row, next_col)
+                    if 0 <= next_row < 8 and 0 <= next_col < 8 and self.board.state.get(next_pos) is None:
+                        self.moves.append(next_pos)
 
 
 class Rook(Piece):
-    def __init__(self, color, x, y):
-        super().__init__(color, x, y)
-
-    def move(self, x, y):
-        return super().move(x, y)
+    def __init__(self, color, x, y, board):
+        super().__init__(color, x, y, board)
+        self.t_color = GREEN
 
     def get_moves(self):
         self.moves.clear()
         op_color = "black" if self.color == "white" else "white"
 
-        move_dir = ["up", "down", "left", "right"]
-        for dir in move_dir:
-            dx, dy = board.dirs[dir]
+        for dir in ["up", "down", "left", "right"]:
+            dr, dc = self.board.dirs[dir]
             step = 1
-            while True:
-                next_pos = (self.x + dx * step, self.y + dy * step)
 
-                if next_pos not in board.state:
+            while True:
+                next_row = self.x + dr * step
+                next_col = self.y + dc * step
+                next_pos = (next_row, next_col)
+
+                # Break if out of bounds
+                if not (0 <= next_row < 8 and 0 <= next_col < 8):
                     break
 
-                if board.state[next_pos] is None:
+                if self.board.state.get(next_pos) is None:
                     self.moves.append(next_pos)
                     step += 1
                     continue
 
-                if board.state[next_pos].color == op_color:
+                if self.board.state[next_pos].color == op_color:
                     self.moves.append(next_pos)
                 break
 
 
 class Bishop(Piece):
-    def __init__(self, color, x, y):
-        super().__init__(color, x, y)
-
-    def move(self, x, y):
-        return super().move(x, y)
+    def __init__(self, color, x, y, board):
+        super().__init__(color, x, y, board)
+        self.t_color = BLUE
 
     def get_moves(self):
         self.moves.clear()
         op_color = "black" if self.color == "white" else "white"
 
-        move_dir = ["top_left", "top_right", "bottom_left", "bottom_right"]
-        for dir in move_dir:
-            dx, dy = board.dirs[dir]
+        for dir in ["top_left", "top_right", "bottom_left", "bottom_right"]:
+            dx, dy = self.board.dirs[dir]
             step = 1
             while True:
-                next_pos = (self.x + dx * step, self.y + dy * step)
+                next_row = self.x + dx * step
+                next_col = self.y + dy * step
 
-                if next_pos not in board.state:
+                if not (0 <= next_row < 8 and 0 <= next_col < 8):
                     break
 
-                if board.state[next_pos] is None:
+                next_pos = (next_row, next_col)
+
+                if self.board.state[next_pos] is None:
                     self.moves.append(next_pos)
                     step += 1
                     continue
 
-                if board.state[next_pos].color == op_color:
+                if self.board.state[next_pos].color == op_color:
                     self.moves.append(next_pos)
                 break
 
 
 class Knight(Piece):
-    def __init__(self, color, x, y):
-        super().__init__(color, x, y)
+    def __init__(self, color, x, y, board):
+        super().__init__(color, x, y, board)
+        self.t_color = PURPLE
 
     def move(self, x, y):
         return super().move(x, y)
@@ -146,22 +152,24 @@ class Knight(Piece):
         move_dir = ["up_left", "up_right", "down_left", 'down_right',
                     "left_up", "left_down", "right_up", "right_down"]
         for dir in move_dir:
-            dx, dy = board.dirs[dir]
-            next_pos = (self.x + dx, self.y + dy)
+            dx, dy = self.board.dirs[dir]
+            next_row = self.x + dx
+            next_col = self.y + dy
 
-            if next_pos not in board.state:
+            next_pos = (next_row, next_col)
+            if not (0 <= next_row < 8 and 0 <= next_col < 8):
                 continue
 
-            if board.state[next_pos] is None:
+            if self.board.state[next_pos] is None:
                 self.moves.append(next_pos)
             else:
-                if board.state[next_pos].color == op_color:
+                if self.board.state[next_pos].color == op_color:
                     self.moves.append(next_pos)
 
 
 class Queen(Piece):
-    def __init__(self, color, x, y):
-        super().__init__(color, x, y)
+    def __init__(self, color, x, y, board):
+        super().__init__(color, x, y, board)
 
     def move(self, x, y):
         return super().move(x, y)
@@ -175,26 +183,28 @@ class Queen(Piece):
 
         for dir in move_dir:
             step = 1
-            dx, dy = board.dirs[dir]
+            dx, dy = self.board.dirs[dir]
             while True:
-                next_pos = (self.x + dx * step, self.y + dy * step)
+                next_row = self.x + dx * step
+                next_col = self.y + dy * step
 
-                if next_pos not in board.state:
+                if not (0 <= next_row < 8 and 0 <= next_col < 8):
                     break
 
-                if board.state[next_pos] is None:
+                next_pos = (next_row, next_col)
+                if self.board.state.get(next_pos) is None:
                     self.moves.append(next_pos)
                     step += 1
                     continue
 
-                if board.state[next_pos].color == op_color:
+                if self.board.state.get(next_pos).color == op_color:
                     self.moves.append(next_pos)
-                    break
+                break
 
 
 class King(Piece):
-    def __init__(self, color, x, y):
-        super().__init__(color, x, y)
+    def __init__(self, color, x, y, board):
+        super().__init__(color, x, y, board)
         self.check = False
 
     def move(self, x, y):
@@ -208,18 +218,24 @@ class King(Piece):
                     "top_right", "bottom_left", "bottom_right"]
 
         for dir in move_dir:
-            dx, dy = board.state[dir]
+            dx, dy = self.board.dirs[dir]
 
-            next_pos = (self.x + dx, self.y + dy)
+            next_row = self.x + dx
+            next_col = self.y + dy
 
-            if next_pos not in board.state:
+            if not (0 <= next_row < 8 and 0 <= next_col < 8):
+                break
+
+            next_pos = (next_row, next_col)
+
+            if next_pos not in self.board.state:
                 continue
 
-            if board.state[next_pos] is None:
+            if self.board.state[next_pos] is None:
                 self.moves.append(next_pos)
                 continue
 
-            if board.state[next_pos].color == op_color:
+            if self.board.state[next_pos].color == op_color:
                 self.moves.append(next_pos)
 
 
@@ -227,45 +243,44 @@ def offset(n):
     return n * UNITS + int(UNITS/2)
 
 
-def initializePieces():
-
+def initializePieces(board):
     for i in range(8):
         # Black Pawns
-        x = offset(i)
-        y = offset(1)
-        board.state[(x, y)] = Pawn("black", x, y)
+        board.state[(1, i)] = Pawn("black", 1, i, board)
 
         # White Pawns
-        y = offset(6)
-        board.state[(x, y)] = Pawn("white", x, y)
+        board.state[(6, i)] = Pawn("white", 6, i, board)
 
     # Rooks
-    board.state[(offset(0), offset(0))] = Rook("black", offset(0), offset(0))
-    board.state[(offset(7), offset(0))] = Rook("black", offset(7), offset(0))
-    board.state[(offset(0), offset(7))] = Rook("white", offset(0), offset(7))
-    board.state[(offset(7), offset(7))] = Rook("white", offset(7), offset(7))
+    board.state[(0, 0)] = Rook("black", 0, 0, board)
+    board.state[(7, 0)] = Rook("black", 7, 0, board)
+    board.state[(0, 7)] = Rook("white", 0, 7, board)
+    board.state[(7, 7)] = Rook("white", 7, 7, board)
 
     # Knights
-    board.state[(offset(1), offset(0))] = Knight("black", offset(1), offset(0))
-    board.state[(offset(6), offset(0))] = Knight("black", offset(6), offset(0))
-    board.state[(offset(1), offset(7))] = Knight("white", offset(1), offset(7))
-    board.state[(offset(6), offset(7))] = Knight("white", offset(6), offset(7))
+    board.state[(0, 1)] = Knight("black", 0, 1, board)
+    board.state[(0, 6)] = Knight("black", 0, 6, board)
+    board.state[(7, 1)] = Knight("white", 7, 1, board)
+    board.state[(7, 6)] = Knight("white", 7, 6, board)
 
     # Bishops
-    board.state[(offset(2), offset(0))] = Bishop("black", offset(2), offset(0))
-    board.state[(offset(5), offset(0))] = Bishop("black", offset(5), offset(0))
-    board.state[(offset(2), offset(7))] = Bishop("white", offset(2), offset(7))
-    board.state[(offset(5), offset(7))] = Bishop("white", offset(5), offset(7))
+    board.state[(0, 2)] = Bishop("black", 0, 2, board)
+    board.state[(0, 5)] = Bishop("black", 0, 5, board)
+    board.state[(7, 2)] = Bishop("white", 7, 2, board)
+    board.state[(7, 5)] = Bishop("white", 7, 5, board)
 
     # Queens
-    board.state[(offset(3), offset(0))] = Queen("black", offset(3), offset(0))
-    board.state[(offset(3), offset(7))] = Queen("white", offset(3), offset(7))
+    board.state[(0, 3)] = Queen("black", 0, 3, board)
+    board.state[(7, 3)] = Queen("white", 7, 3, board)
 
     # Kings
-    board.state[(offset(4), offset(0))] = King("black", offset(4), offset(0))
-    board.state[(offset(4), offset(7))] = King("white", offset(4), offset(7))
+    board.state[(0, 4)] = King("black", 0, 4, board)
+    board.state[(7, 4)] = King("white", 7, 4, board)
+
+    pprint.pprint(board.state)
 
 
-def initializeTextures():
-    for _, pieces in board.state.items():
-        pieces.init_texture()
+def initializeTextures(board):
+    for _, piece in board.state.items():
+        if piece is not None:
+            piece.init_texture()
